01b9d4b991e84e64ae7f4591cc52a84dfaf98ba9
Carlos Henrique <chmmaestro@gmail.com>
2018-05-28 20:28:16 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/chmmaestro/maisvidabackend/MaisvidabackendApplication.java
@@ -0,0 +1,12 @@
+package com.chmmaestro.maisvidabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisvidabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisvidabackendApplication.class, args);
+	}
+}
