d5b78427454d65b4822b9d16b36fa69e3a5ee41a
Carlos Henrique <chmmaestro@gmail.com>
2018-05-28 22:21:53 -0300

    obtendo usuario por id e tratamento de erro

---
+++ src/main/java/com/chmmaestro/maisvidabackend/exception/ObjectNotFounException.java
@@ -0,0 +1,11 @@
+package com.chmmaestro.maisvidabackend.exception;
+
+public class ObjectNotFounException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFounException(String msg) {
+		super(msg);
+	}
+	
+}
--- src/main/java/com/chmmaestro/maisvidabackend/services/UserService.java
+++ src/main/java/com/chmmaestro/maisvidabackend/services/UserService.java
@@ -1,22 +1,28 @@
 package com.chmmaestro.maisvidabackend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.chmmaestro.maisvidabackend.domain.User;
+import com.chmmaestro.maisvidabackend.exception.ObjectNotFounException;
 import com.chmmaestro.maisvidabackend.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	 
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFounException("Objeto não encontrado"));
+	}
+
 }
