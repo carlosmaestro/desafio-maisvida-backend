27685dc18ddeae584c2f453ae740407b27ef098a
Carlos Henrique <chmmaestro@gmail.com>
2018-05-28 21:54:17 -0300

    padrão dto para usuarios

--- src/main/java/com/chmmaestro/maisvidabackend/resources/UserResources.java
+++ src/main/java/com/chmmaestro/maisvidabackend/resources/UserResources.java
@@ -1,8 +1,7 @@
 package com.chmmaestro.maisvidabackend.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chmmaestro.maisvidabackend.domain.User;
+import com.chmmaestro.maisvidabackend.dto.UserDTO;
 import com.chmmaestro.maisvidabackend.services.UserService;
 
 @RestController
@@ -21,11 +21,11 @@ public class UserResources {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = service.findAll();
-
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
